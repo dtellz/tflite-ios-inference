183a81b4254b74dae6cfa3673a8a101ff4234074
Diego <dtellez95@gmail.com>
2023-06-10 20:10:25 +0100

    initial commit

---
+++ tflite-ios-inferenceUITests/tflite_ios_inferenceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tflite_ios_inferenceUITestsLaunchTests.swift
+//  tflite-ios-inferenceUITests
+//
+//  Created by Diego Tellez Barrero on 10/6/23.
+//
+
+import XCTest
+
+final class tflite_ios_inferenceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
